7b28ba69cd1789b7234ed31bea9a1361a30bfe42
Thalia Villalobos <vill9422@m506-246-25987.local>
2018-02-20 20:09:02 -0800

    user is able to upload image and text

--- instagram/ImportImageViewController.swift
+++ instagram/ImportImageViewController.swift
@@ -6,15 +6,18 @@
 //  Copyright © 2018 Thalia Villalobos. All rights reserved.
 //
 
+//************Fix image, does not display in database*************
+
+
 import UIKit
 import Parse
 
-class ImportImageViewController: UIViewController, UINavigationControllerDelegate, UIImagePickerControllerDelegate {
+class ImportImageViewController: UIViewController, UINavigationControllerDelegate, UIImagePickerControllerDelegate{
 
     @IBOutlet weak var userImageView: UIImageView!
     @IBOutlet weak var capationTextField: UITextField!
 
- 
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -42,14 +45,43 @@ class ImportImageViewController: UIViewController, UINavigationControllerDelegat
     
     
     @IBAction func submitImage(_ sender: AnyObject) {
-    
+        let imageText = capationTextField.text
+        
+        if userImageView.image == nil{
+            //image is not includeded alert user
+            print("Image not uploaded")
+        }else{
+            let posts = PFObject(className: "Posts")
+            posts["imageText"] = imageText
+            posts["user"] = PFUser.current()
+            posts.saveInBackground(block: { (sccess:Bool, error: NSError?) -> Void in
+                if error == nil{
+                    //create an image data
+                    let imageData = UIImagePNGRepresentation(self.userImageView.image!)
+                    let parseImageFile = PFFile(name:"uploaded_image.png", data: imageData!)
+                    posts["imageFile"] = parseImageFile
+                    posts.saveInBackground(block: { (sccess:Bool, error: NSError?) -> Void in
+                        if error == nil{
+                            print ("data uploaded")
+                        }else{
+                            print("error")
+                        }
+                    } as? PFBooleanResultBlock)
+                }
+            } as? PFBooleanResultBlock)
+        }
     }
 
+    @IBAction func backHome(_ sender: AnyObject) {
+        performSegue(withIdentifier: "goHome", sender: self)
+    }
+    
+    
     func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
         if let image = info[UIImagePickerControllerOriginalImage] as? UIImage{
             userImageView.image = image
         }else{
-            //error message
+            print("Something went wrong")
         }
         
         self.dismiss(animated: true, completion: nil)
